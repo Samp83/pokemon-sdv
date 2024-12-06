52d52ed422862d9958d375c44f2289be5c7da738
Samp83 <sami.paris@supdevinci-edu.fr>
2024-12-06 15:11:16 +0100

    app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-//import Header from './Header.jsx'
-//import Footer from './Footer.jsx'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Welcome from './pages/Welcome'
